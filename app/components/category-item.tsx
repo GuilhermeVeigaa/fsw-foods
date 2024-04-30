5d2ab594c726474673587864b5993fdf52369670
GuilhermeVeigaa <guilhermeveigafff@gmail.com>
2024-04-30 19:16:10 -0300

    feat: add category list

--- app/components/category-item.tsx
+++ app/components/category-item.tsx
@@ -7,7 +7,7 @@ interface CategoryItemProps {
 
 export default function CategoryItem({ category }: CategoryItemProps) {
   return (
-    <div className="flex items-center gap-3 rounded-full bg-white px-4 py-3 shadow-md">
+    <div className="mb-3 flex min-w-[180px] items-center gap-3 rounded-full bg-white px-4 py-3 shadow-lg">
       <Image
         src={category.imageUrl}
         alt={category.name}
--- app/components/category-list.tsx
+++ app/components/category-list.tsx
@@ -5,7 +5,7 @@ export default async function CategoryList() {
   const categories = await db.category.findMany({});
 
   return (
-    <div className="grid grid-cols-2 gap-3">
+    <div className="flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
       {categories.map((category) => (
         <CategoryItem key={category.id} category={category} />
       ))}
